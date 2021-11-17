ef80815745b2020ac2cc3ce645003fd21c1e058e
Ronei Simões Calistro <ronei.calistro@ALFA.local>
2021-11-17 11:15:50 -0300

    Finalizando o CRUD

--- chamados/urls.py
+++ chamados/urls.py
@@ -2,11 +2,41 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #rotas chamados
     path('', views.home, name='home'),
-    path('form/', views.form, name='form'),
-    path('create/', views.create, name='create'),
-    path('view/<int:pk>/', views.view, name='view'),
-    path('edit/<int:pk>/', views.edit, name='edit'),
-    path('update/<int:pk>/', views.update, name='update'),
-    path('delete/<int:pk>/', views.delete, name='delete'),
+
+    path('chamado/', views.home_chamado, name='home_chamado'),
+    path('form_chamado/', views.form_chamado, name='form_chamado'),
+    path('create_chamado/', views.create_chamado, name='create_chamado'),
+    path('view_chamado/<int:pk>/', views.view_chamado, name='view_chamado'),
+    path('edit_chamado/<int:pk>/', views.edit_chamado, name='edit_chamado'),
+    path('update_chamado/<int:pk>/', views.update_chamado, name='update_chamado'),
+    path('delete_chamado/<int:pk>/', views.delete_chamado, name='delete_chamado'),
+
+    #rotas status
+    path('status/', views.home_status, name='home_status'),
+    path('form_status/', views.form_status, name='form_status'),
+    path('create_status/', views.create_status, name='create_status'),
+    path('view_status/<int:pk>/', views.view_status, name='view_status'),
+    path('edit_status/<int:pk>/', views.edit_status, name='edit_status'),
+    path('update_status/<int:pk>/', views.update_status, name='update_status'),
+    path('delete_status/<int:pk>/', views.delete_status, name='delete_status'),
+
+    #rotas categoria
+    path('categoria/', views.home_categoria, name='home_categoria'),
+    path('form_categoria/', views.form_categoria, name='form_categoria'),
+    path('create_categoria/', views.create_categoria, name='create_categoria'),
+    path('view_categoria/<int:pk>/', views.view_categoria, name='view_categoria'),
+    path('edit_categoria/<int:pk>/', views.edit_categoria, name='edit_categoria'),
+    path('update_categoria/<int:pk>/', views.update_categoria, name='update_categoria'),
+    path('delete_categoria/<int:pk>/', views.delete_categoria, name='delete_categoria'),
+
+    #rotas pessoa
+    path('pessoa/', views.home_pessoa, name='home_pessoa'),
+    path('form_pessoa/', views.form_pessoa, name='form_pessoa'),
+    path('create_pessoa/', views.create_pessoa, name='create_pessoa'),
+    path('view_pessoa/<int:pk>/', views.view_pessoa, name='view_pessoa'),
+    path('edit_pessoa/<int:pk>/', views.edit_pessoa, name='edit_pessoa'),
+    path('update_pessoa/<int:pk>/', views.update_pessoa, name='update_pessoa'),
+    path('delete_pessoa/<int:pk>/', views.delete_pessoa, name='delete_pessoa'),
 ]
--- chamados/views.py
+++ chamados/views.py
@@ -1,49 +1,218 @@
 from django.shortcuts import render, redirect
-from chamados.forms import CategoriaForm
-from chamados.models import Categoria
+from chamados.forms import CategoriaForm, ChamadoForm, StatusForm, PessoaForm
+from chamados.models import Categoria, Chamado, Status, Pessoa
 from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
+    return render(request, 'index.html')
+
+# Categoria
+def home_categoria(request):
     dados = {}
-    #dados['db'] = Categoria.objects.all()
-    all = Categoria.objects.all()
-    paginator = Paginator(all,3)
-    pages = request.GET.get('page')
-    dados['db'] = paginator.get_page(pages)
-    return render(request, 'index.html', dados)
+    search = request.GET.get('search')
+    if search:
+        dados['db'] = Categoria.objects.filter(nome__icontains=search)
+    else:
+        dados['db'] = Categoria.objects.all()
 
-def form(request):
+    #paginator
+    #all = Categoria.objects.all()
+    #paginator = Paginator(all,3)
+    #pages = request.GET.get('page')
+    #dados['db'] = paginator.get_page(pages)
+    return render(request, 'categoria.html', dados)
+
+def form_categoria(request):
     data = {}
-    data['form'] = CategoriaForm()
-    return render(request, 'form.html', data)
+    data['form_categoria'] = CategoriaForm()
+    return render(request, 'form_categoria.html', data)
 
-def create(request):
+def create_categoria(request):
     form = CategoriaForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect('home')
+        return redirect('home_categoria')
 
-def view(request, pk):
+def view_categoria(request, pk):
     data = {}
     data['db'] = Categoria.objects.get(pk=pk)
-    return render(request, 'view.html', data)
+    return render(request, 'view_categoria.html', data)
 
-def edit(request, pk):
+def edit_categoria(request, pk):
     data = {}
     data['db'] = Categoria.objects.get(pk=pk)
-    data['form'] = CategoriaForm(instance=data['db'])
-    return render(request, 'form.html', data)
+    data['form_categoria'] = CategoriaForm(instance=data['db'])
+    return render(request, 'form_categoria.html', data)
 
-def update(request, pk):
+def update_categoria(request, pk):
     data = {}
     data['db'] = Categoria.objects.get(pk=pk)
     form = CategoriaForm(request.POST or None, instance=data['db'])
     if form.is_valid():
         form.save()
-        return redirect('home')
+        return redirect('home_categoria')
 
-def delete(request, pk):
+def delete_categoria(request, pk):
     db = Categoria.objects.get(pk=pk)
     db.delete()
-    return redirect('home')
+    return redirect('home_categoria')
+
+#=====================================
+#Status
+#=====================================
+def home_status(request):
+    dados = {}
+    search = request.GET.get('search')
+    if search:
+        dados['db'] = Status.objects.filter(nome__icontains=search)
+    else:
+        dados['db'] = Status.objects.all()
+
+    #paginator
+    #all = Categoria.objects.all()
+    #paginator = Paginator(all,3)
+    #pages = request.GET.get('page')
+    #dados['db'] = paginator.get_page(pages)
+    return render(request, 'status.html', dados)
+
+def form_status(request):
+    data = {}
+    data['form_status'] = StatusForm()
+    return render(request, 'form_status.html', data)
+
+def create_status(request):
+    form = StatusForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home_status')
+
+def view_status(request, pk):
+    data = {}
+    data['db'] = Status.objects.get(pk=pk)
+    return render(request, 'view_status.html', data)
+
+def edit_status(request, pk):
+    data = {}
+    data['db'] = Status.objects.get(pk=pk)
+    data['form_status'] = StatusForm(instance=data['db'])
+    return render(request, 'form_status.html', data)
+
+def update_status(request, pk):
+    data = {}
+    data['db'] = Status.objects.get(pk=pk)
+    form = StatusForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home_status')
+
+def delete_status(request, pk):
+    db = Status.objects.get(pk=pk)
+    db.delete()
+    return redirect('home_status')
+
+#======================================
+#Pessoa
+#======================================
+def home_pessoa(request):
+    dados = {}
+    search = request.GET.get('search')
+    if search:
+        dados['db'] = Pessoa.objects.filter(nome__icontains=search)
+    else:
+        dados['db'] = Pessoa.objects.all()
+
+    #paginator
+    #all = Categoria.objects.all()
+    #paginator = Paginator(all,3)
+    #pages = request.GET.get('page')
+    #dados['db'] = paginator.get_page(pages)
+    return render(request, 'pessoa.html', dados)
+
+def form_pessoa(request):
+    data = {}
+    data['form_pessoa'] = PessoaForm()
+    return render(request, 'form_pessoa.html', data)
+
+def create_pessoa(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home_pessoa')
+
+def view_pessoa(request, pk):
+    data = {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    return render(request, 'view_pessoa.html', data)
+
+def edit_pessoa(request, pk):
+    data = {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    data['form_pessoa'] = PessoaForm(instance=data['db'])
+    return render(request, 'form_pessoa.html', data)
+
+def update_pessoa(request, pk):
+    data = {}
+    data['db'] = Pessoa.objects.get(pk=pk)
+    form = PessoaForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home_pessoa')
+
+def delete_pessoa(request, pk):
+    db = Pessoa.objects.get(pk=pk)
+    db.delete()
+    return redirect('home_pessoa')
+
+#======================================
+#Chamados
+#======================================
+def home_chamado(request):
+    dados = {}
+    search = request.GET.get('search')
+    if search:
+        dados['db'] = Chamado.objects.filter(titulo__icontains=search)
+    else:
+        dados['db'] = Chamado.objects.all()
+
+    #paginator
+    #all = Categoria.objects.all()
+    #paginator = Paginator(all,3)
+    #pages = request.GET.get('page')
+    #dados['db'] = paginator.get_page(pages)
+    return render(request, 'chamado.html', dados)
+
+def form_chamado(request):
+    data = {}
+    data['form_chamado'] = ChamadoForm()
+    return render(request, 'form_chamado.html', data)
+
+def create_chamado(request):
+    form = ChamadoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home_chamado')
+
+def view_chamado(request, pk):
+    data = {}
+    data['db'] = Chamado.objects.get(pk=pk)
+    return render(request, 'view_chamado.html', data)
+
+def edit_chamado(request, pk):
+    data = {}
+    data['db'] = Chamado.objects.get(pk=pk)
+    data['form_chamado'] = ChamadoForm(instance=data['db'])
+    return render(request, 'form_chamado.html', data)
+
+def update_chamado(request, pk):
+    data = {}
+    data['db'] = Chamado.objects.get(pk=pk)
+    form = ChamadoForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home_chamado')
+
+def delete_chamado(request, pk):
+    db = Chamado.objects.get(pk=pk)
+    db.delete()
+    return redirect('home_chamado')
